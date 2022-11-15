608423833093736d037246609e8b0f940c3cc75f
leon0X01 <ushrestha358@gmail.com>
2022-11-15 18:05:18 +0545

    added to view price and calculation AMM

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -28,6 +28,47 @@ contract Exchange{
         returns (uint256)
     {
         require(inputReserve > 0 && outputReserve > 0, "invalid reserves");
-        return inputReserve / outputReserve;
+        return (inputReserve * 1000) / outputReserve;
     }
+    
+    function getAmount(
+        uint256 inputAmount,
+        uint256 inputReserve,
+        uint256 outputReserve
+    ) private pure returns (uint256){
+        require(inputReserve > 0 && outputReserve > 0, "invalid reserves");
+        return (inputAmount * outputReserve) / (inputReserve + inputAmount);
+    }
+
+    function getTokenAmount(
+        uint256 _ethSold) public view returns (uint256) {
+            require(_ethSold > 0, "ethSold is too small");
+            uint256 tokenReserve = getReserve();
+            return getAmount(_ethSold, address(this).balance, tokenReserve);
+        }
+    function getEthAmount(
+        uint256 _tokenSold) public view returns 
+        (uint256){
+            require(_tokenSold > 0, "tokenSold is too small");
+            uint256 tokenReserve = getReserve();
+            return getAmount(_tokensold, tokenReserve, address(this).balance);
+        }
+    function ethToTokenSwap(int256 _minTokens) public payable {
+        uint256 tokenReserve = getReserve();
+        uint256 tokensBought = getAmount(
+            msg.value,
+            address(this).balance - msg.value,
+            tokenReserve
+        );
+        require(tokensBought >= _minTokens, "insufficent output amount");
+        IERC20(tokenAddress).transfer(msg.sender, tokensBought);
+    }
+
+    function tokenToEthSwap(uint256 _tokensSold, uint256 _minEth) public {
+        uint256 tokenReserve = getReserve();
+        uint256 ethBought = getAmount(_tokenSold, tokenReserve, address(this).balance);
+    };
+    require(ethBought >= _minEth, "insufficient output amount");
+    IERC20(tokenAddress).transferFrom(msg.sender, address(this), _tokenSold);
+    payable(msg.sender).transfer(ethBought);
 }
